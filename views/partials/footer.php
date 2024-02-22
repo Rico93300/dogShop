be29bbcc88b38220e4ddb14dc08c42002f8d9dea
Aziza S <azizaalgerie@hotmail.fr>
2024-02-22 15:25:28 +0100

    Footer php fini

---
+++ views/partials/footer.php
@@ -0,0 +1,25 @@
+<footer>
+        <div class="row">
+            <div class="col-lg-4 text-center">
+                <img src="/img/paimentsSecurisés.png" style="height: 50px;" alt=""/>
+                <p>PAIEMENTS SÉCURISÉS CB,<br> 
+                    PAYPAL, CHÈQUE</p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4 text-center">
+                <img src="/img/livraison.png" style="height: 50px;" alt=""/>
+                <p>LIVRAISON RAPIDE ET <br>
+                    GRATUITE DÈS 35€ D'ACHAT</p>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-lg-4 text-center">
+                <img src="/img/achatSatisfaitOuRemboursé.png" style="height: 50px;" alt=""/>
+                <p>ACHAT SATISFAIT OU <br> 
+                    REMBOURSÉ 15 JOURS</p>
+            </div><!-- /.col-lg-4 -->
+        </div>
+
+        <div class="d-flex align-items-end  bg-dark"  style="height: 240px">
+            <div class="text-center mx-auto text-white"> © <?= date('Y') ?> Copyright - DogShop</div>
+        </div>
+    </footer>
+</body>
+</html>
