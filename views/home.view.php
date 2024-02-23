fade2ed2b021d03e851c683124a302a9419e06b1
Aziza S <azizaalgerie@hotmail.fr>
2024-02-23 16:43:16 +0100

    ajout nouveauté

--- views/home.view.php
+++ views/home.view.php
@@ -6,7 +6,16 @@
 
 <div class="container">
     <img src="/img/chienBanner.png" class="col-lg-12" style="height: 375px;" alt="banner avec un chien">
+    <div class="d-flex">
+                    <div class="heigth_tittle bg-warning text-dark"></div>
+                    <div><h3>NOUVEAUTÉS</h3></div>
+                    <div class="heigth_tittle bg-warning text-dark"></div>
+    </div>
 </div>
+
+
+
+
 <div class="min-vh-100"></div>
 
 <?php require_once(__DIR__."/partials/footer.php") ?>
