76ef311d41970ed16b3a327ae62792ce970c023d
Aziza S <azizaalgerie@hotmail.fr>
2024-02-23 11:08:00 +0100

    Ajout mis a jour du head

--- views/partials/head.php
+++ views/partials/head.php
@@ -16,7 +16,7 @@
             <a class="navbar-brand" href="#">
                 <img src="/img/logo.png" alt="logo patte de chien marron" height="50"></a>
                 <li class="nav-item">
-                    <a class="nav-link text-secondary" href="/" aria-current="page">ACCUEIL</a>
+                    <a class="nav-link active text-secondary" href="/" aria-current="page">ACCUEIL</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-secondary" href="">NOURRITURE</a>
