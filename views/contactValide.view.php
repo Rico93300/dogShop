7f4c1a8f6d7438f21ad51f7d7e5a06df65c1a44c
Aziza <azizaalgerie@hotmail.fr>
2024-02-25 22:23:51 +0100

    Ajout  css/homeContact.css et modification multiple   css/nouriturre.css   contact.view.php   contactValide.view.php   home.view.php   jouet.view.php

--- views/contactValide.view.php
+++ views/contactValide.view.php
@@ -2,15 +2,19 @@
 $title = "Validation contact";
 require_once(__DIR__."/partials/head.php")?>
 
+<link rel="stylesheet" href="/css/homeContact.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Preahvihear&display=swap" rel="stylesheet">
 
 
-    <h1 class="text-center m-5">Merci d’avoir pris le temps de nous contacter via notre formulaire</h1>
+    <h2 class="text-center m-5">Merci d’avoir pris le temps de nous contacter via notre formulaire</h2>
     <div class="text-center mb-4">
         <div>
             <div class="col-lg-12 ">
                 <img src="/img/chiens.png" style="width: 50%;" alt="Photo de plusieurs chiens" >
             </div>
-            <h5 class="mb-5">Votre message a bien été transmis à nos équipes, nous vous répondrons dans les plus brefs délais.</h5>
+            <p class="mb-5">Votre message a bien été transmis à <br> nos équipes, nous vous répondrons <br> dans les plus brefs délais</p>
         </div>
     </div>
 
